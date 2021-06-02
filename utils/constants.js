470c8be4072f233b7481968f80ae56d65c818d97
Bram ten Cate <btencate@sphereon.com>
2021-06-02 17:06:02 +0200

    WIP

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,17 @@
 'use strict';
 
-module.exports.DID_LTO_METHOD_PATTERN = '^did:lto:.+';
-module.exports.URL_MAINNET = 'https://lto.network';
-module.exports.URL_TESTNET = 'https://testnet.lto.network';
-module.exports.MAINNET_KEY = 'mainnet';
-module.exports.TESTNET_KEY = 'testnet';
-module.exports.RESOLVE_ENDPOINT = '/did/';
+const DID_LTO_METHOD_PATTERN = '^did:lto:.+';
+const URL_MAINNET = 'https://lto.network';
+const URL_TESTNET = 'https://testnet.lto.network';
+const MAINNET_KEY = 'mainnet';
+const TESTNET_KEY = 'testnet';
+const RESOLVE_ENDPOINT = '/did/';
+
+module.exports = {
+  DID_LTO_METHOD_PATTERN,
+  URL_MAINNET,
+  URL_TESTNET,
+  MAINNET_KEY,
+  TESTNET_KEY,
+  RESOLVE_ENDPOINT,
+};
