8f597e162ae8c0f6f6f7f3a0ba743537bf05294d
Bram ten Cate <btencate@sphereon.com>
2021-06-03 12:34:29 +0200

    WIP

--- service/didService.js
+++ service/didService.js
@@ -83,8 +83,7 @@ const getTargetIdentifier = identifier => {
  * returns DID document object
  **/
 const getDidDocument = (nodeAddress, identifier) => {
-  //const url = nodeAddress + constants.RESOLVE_ENDPOINT + identifier;
-  const url = 'https://api.github.com/repos/atom/atom/license';
+  const url = nodeAddress + constants.RESOLVE_ENDPOINT + identifier;
   return axios.get(url)
     .then(response => response.data)
 };
